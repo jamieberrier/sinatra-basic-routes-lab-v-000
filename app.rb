ac4164d76607e3fba520a72bf02475b258f66684
jamieberrier <berrier.jamie@gmail.com>
2019-07-01 21:55:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,12 @@ class App < Sinatra::Base
   get '/name' do
     "My name is __"
   end
+
+  get '/hometown' do
+    "My hometown is __"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is __"
+  end
 end
