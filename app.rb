a13d3aac5e1b326066e5deb008879ba43355b845
jamieberrier <berrier.jamie@gmail.com>
2019-07-01 21:56:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    "My name is __"
+    "My name is Jamie"
   end
 
   get '/hometown' do
-    "My hometown is __"
+    "My hometown is Chandler, AZ"
   end
 
   get '/favorite-song' do
-    "My favorite song is __"
+    "My favorite song is 'I Want To Dance With Somebody'"
   end
-  
+
 end
